cada10921c31cf8f8532a39d37fa313c1d156d72
senior <senior@cc.lan>
2021-04-05 15:23:38 +0300

    python 2.7 compat

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import time
 from typing import cast
-from unittest import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
 
 import pytest
 from redis import Redis
